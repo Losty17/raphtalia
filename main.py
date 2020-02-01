a708056165d0fd460f160bfb7da0202ae227b325
Losty <vinikappke@gmail.com>
2020-02-01 17:00:24 -0300

    Hello

--- main.py
+++ main.py
@@ -1,107 +1,69 @@
 import discord
 import asyncio
-import secret
+import time
+from discord.ext import commands
 from random import randint
-
-client = discord.Client()
-guild = discord.Guild
+import secret
 
 COR = 0xF26DDC 
-
 TOKEN = secret.token()
-msg_id = None
-msg_user = None
+OWNER = secret.owner()
+BOT = secret.bot()
 
-@client.event
+game = discord.Game('my prefix is "!"')
+
+global bot
+bot = commands.Bot(command_prefix = '!', help_command = None, case_insensitive = True, owner_id = OWNER)
+
+@bot.event
 async def on_ready():
-    print('OLA MUNDO!')
-    await client.change_presence(activity = discord.Game('My prefix is "?" uwu'), status = discord.Status.idle)
+    print('Olá Mundo!')
+    await bot.change_presence(
+        activity = game, 
+        status = discord.Status.idle
+        )
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.content.lower().startswith('?help') or message.content.lower().startswith('?ajuda'):
-        ajuda = discord.Embed(
-            title = "Comandos: ",
-            color = COR,
-            description = "\n?help \n?teste \n?moeda"
-        )
-        await message.channel.send(embed = ajuda)
+    if bot.user.mentioned_in(message) and not message.content.startswith('!') and message.author != bot.user:
+        await message.channel.send('Meu prefixo é "!" u.u')
+    if 'pindamonhangaba' in message.content and not message.content.startswith('!'):
+        await message.channel.send('TALOCO É?')
 
-    if message.content.lower().startswith('?teste'):
-        await message.channel.send('Hello world')
+    await bot.process_commands(message)
 
-    if message.content.lower().startswith('?moeda'):
-        if message.author.id == 207947146371006464:
-            choice = randint(0,1)
-            if choice == 0:
-                await message.channel.send('Cara!')
-                await message.add_reaction('😀')
-            if choice == 1:
-                await message.channel.send('Coroa!')
-                await message.add_reaction('👑')
-        else:
-            await message.channel.send('Você não tem permissão para usar este comando!')
+@bot.command(pass_context=True)
+async def ping(message):
+    mention_author = '{0.author.mention}'.format(message)
+    ms = str(int(bot.latency * 1000))
+    await message.channel.send(mention_author + ' Pong! Minha latência é de ' + ms + 'ms')
 
-    if message.content.lower().startswith('?roles'):
-        cargos = discord.Embed(
-            title = "Selecione seus cargos!",
-            color = COR,
-            description = "» Programador - 👩‍💻\n» Artista - 👩‍🎨\n» Sonoplasta - 👩‍🎤",
-        )
-        botmsg = await message.channel.send(embed = cargos)
-        await botmsg.add_reaction('👩‍💻')
-        await botmsg.add_reaction('👩‍🎨')
-        await botmsg.add_reaction('👩‍🎤')
+@bot.command()
+async def help(message):
+    mention_author = '{0.author.mention}'.format(message)
+    ajuda = discord.Embed(
+        title = "Comandos: ",
+        color = COR,
+        description = "\n!help \n!teste \n!moeda"
+    )
+    await message.channel.send(mention_author, embed = ajuda)
 
-        global msg_id
-        msg_id = botmsg.id
-        global msg_user
-        msg_user = message.author
+@bot.command()
+async def moeda(message):
+    mention_author = '{0.author.mention}'.format(message)
+    choice = randint(0,1)
+    if choice == 0:
+        await message.channel.send(mention_author + ' Cara!')
+    if choice == 1:
+        await message.channel.send(mention_author + ' Coroa!')
 
-#### ?roles command, just works for https://discord.gg/dZMCKP7
-@client.event
-async def on_raw_reaction_add(payload):
-    message_id = msg_id
-    if message_id == msg_id:
-        myRole = None
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+@bot.command(pass_context = True)
+async def say(ctx, *args):
+    mesg = ' '.join(args)
+    if mesg == 'pindamonhangaba':
+        await ctx.message.channel.send('Achou que eu ia falar? bobinho')
+    else:
+        await ctx.message.delete()
+        await ctx.message.channel.send(mesg)
 
-        if payload.emoji.name == '👩‍💻':
-            myRole = discord.utils.get(guild.roles, name='💻dev')
-
-        elif payload.emoji.name == '👩‍🎨':
-            myRole = discord.utils.get(guild.roles, name='🎨artista')
-
-        elif payload.emoji.name == '👩‍🎤':
-            myRole = discord.utils.get(guild.roles, name='📢sonoplasta')
-
-        if myRole != None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member != None and member != 672901764747493397:
-                await member.add_roles(myRole)
-
-@client.event
-async def on_raw_reaction_remove(payload):
-    message_id = msg_id
-    if message_id == msg_id:
-        myRole = None
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
-
-        if payload.emoji.name == '👩‍💻':
-            myRole = discord.utils.get(guild.roles, name='💻dev')
-
-        elif payload.emoji.name == '👩‍🎨':
-            myRole = discord.utils.get(guild.roles, name='🎨artista')
-
-        elif payload.emoji.name == '👩‍🎤':
-            myRole = discord.utils.get(guild.roles, name='📢sonoplasta')
-
-        if myRole != None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member != 672901764747493397 and member != None:
-                await member.remove_roles(myRole)
-###
-
-client.run(TOKEN)
+bot.run(TOKEN)
--- secret.py
+++ secret.py
@@ -1,2 +1,8 @@
 def token():
     return 'NjcyOTAxNzY0NzQ3NDkzMzk3.XjSOvQ.mgQZixoOI2-iM8jHrVZfDbeeBfg'
+
+def owner():
+    return '207947146371006464'
+
+def bot():
+    return '672901764747493397'
