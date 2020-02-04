b49ed8779f5670d7f960b08e48675aa5033539e3
Losty <vinikappke@gmail.com>
2020-02-04 20:06:36 -0300

    adasd

--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
 def token():
-    return 'NjcyOTAxNzY0NzQ3NDkzMzk3.XjSOvQ.mgQZixoOI2-iM8jHrVZfDbeeBfg'
+    return 'NjcyOTAxNzY0NzQ3NDkzMzk3.XjnOtQ.mraw1HBe4VfBeIjopjd9osC6KQg'
 
 def owner():
     return 207947146371006464
