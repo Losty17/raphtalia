974bb3d4bde10c85c7fe9efccf6876631be24077
Vinicius Kappke <vinikappke@gmail.com>
2020-04-17 13:45:13 -0300

    yey

--- modules/error_handler.py
+++ modules/error_handler.py
@@ -9,21 +9,19 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.NoPrivateMessage):
-            await ctx.send('Desculpe, mas é necessário estar em um servidor para utilizar meus comandos.')
-        elif isinstance(error, nekos.errors.NothingFound):
-            return
+            return await ctx.send('Desculpe, mas é necessário estar em um servidor para utilizar meus comandos.')
         elif isinstance(error, commands.DisabledCommand):
-            await ctx.send('Este comando está desabilitado temporariamente.')
+            return await ctx.send('Este comando está desabilitado temporariamente.')
         elif isinstance(error, commands.errors.NSFWChannelRequired):
-            await ctx.send(f'Você so pode usar este comando em um canal NSFW!')
+            return await ctx.send(f'Você so pode usar este comando em um canal NSFW!')
         elif isinstance(error, commands.CheckFailure):
-            await ctx.send('Você não tem permissão para acessar este comando!')
+            return await ctx.send('Você não tem permissão para acessar este comando!')
         elif isinstance(error, commands.CommandNotFound):
             return
             #await ctx.send('Desculpe, não consegui encontrar o comando solicitado...')
         elif isinstance(error, commands.errors.CommandInvokeError):
-            await ctx.send(f'Ops, acabei encontrando um erro!\n<@!207947146371006464>```{error}```')
-        
+            #await ctx.send(f'Ops, acabei encontrando um erro!\n<@!207947146371006464>```{error}```')
+            return
 
 def setup(bot):
     bot.add_cog(Error(bot))
